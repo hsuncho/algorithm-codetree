0c13fbc8d49159ad0542611376f4eb4f713db31f
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-04-25 22:30:38 +0900

    Title: 연속되는 수 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/연속되는 수 2/continuous-number2.py
+++ 260425/연속되는 수 2/continuous-number2.py
@@ -1,10 +1,15 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
-cnt_arr = []
+
 cnt = 1
+max_cnt = 1
+
 for i in range(1, n):
     if arr[i] == arr[i-1]:
         cnt += 1
-    else: cnt_arr.append(cnt)
+    else:
+        cnt = 1
+    
+    max_cnt = max(max_cnt, cnt)
 
-print(max(cnt_arr))
+print(max_cnt)
