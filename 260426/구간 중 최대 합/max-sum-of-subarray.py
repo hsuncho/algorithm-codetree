60f481a7667ac51970cf5b6baccf61eceacc2ee2
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-04-26 21:52:13 +0900

    Title: 구간 중 최대 합, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260426/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+total = 0
+max_sum = 0
+for i in range(n-k):
+    total = sum(arr[i:i+k])
+    max_sum = max(max_sum, total)
+
+print(max_sum)
