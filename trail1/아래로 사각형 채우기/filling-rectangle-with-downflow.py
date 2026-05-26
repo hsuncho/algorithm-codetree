c5d6fbb7fcd9673196b594ff56457fee6909b3e2
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-05-26 18:44:59 +0900

    [Passed] 아래로 사각형 채우기 (52ms, 16MB)

--- trail1/아래로 사각형 채우기/filling-rectangle-with-downflow.py
+++ trail1/아래로 사각형 채우기/filling-rectangle-with-downflow.py
@@ -4,4 +4,14 @@ arr_2d = [
     [0 for _ in range(n)]
     for _ in range(n)
 ]
-print(arr_2d)
+
+num = 1
+for i in range(n):
+    for j in range(n):
+        arr_2d[j][i] = num
+        num += 1
+
+for row in arr_2d:
+    for elem in row:
+        print(elem, end=" ")
+    print()
