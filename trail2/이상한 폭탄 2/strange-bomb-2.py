abee7f552ced41b387b50c5d9aface29f90b07b4
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-06-27 18:00:37 +0900

    [Passed] 이상한 폭탄 2 (59ms, 17MB)

--- trail2/이상한 폭탄 2/strange-bomb-2.py
+++ trail2/이상한 폭탄 2/strange-bomb-2.py
@@ -4,13 +4,10 @@ num = [int(input()) for _ in range(N)]
 # Please write your code here.
 ans = -1
 for i in range(N):
-    cnt = 0
     for j in range(N):
         if i == j:
             continue
 
         if num[i] == num[j] and abs(i - j) <= K:
-            cnt = num[i]
-    
-    ans = max(cnt, ans)
+            ans = max(num[i], ans)
 print(ans)
