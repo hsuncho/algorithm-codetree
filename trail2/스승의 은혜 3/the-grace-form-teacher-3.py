312f3133a41dd7619f3741d8ec0936aec3466c36
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-06-27 18:34:10 +0900

    [Passed] 스승의 은혜 3 (54ms, 16MB)

--- trail2/스승의 은혜 3/the-grace-form-teacher-3.py
+++ trail2/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -4,21 +4,31 @@ P = [gift[0] for gift in gifts]
 S = [gift[1] for gift in gifts]
 
 # Please write your code here.
-prices = [P[k] + S[k] for k in range(N)]
 ans = 0
 for i in range(N):
-    P[i] /= 2
-    prices[i] = P[i] + S[i]
-    prices.sort()
+    prices = []
+    for j in range(N):
+        
+        p = P[j]
+        s = S[j]
+
+        if i == j:
+            cost = p // 2 + s
+        else:
+            cost = p + s
+        
+        prices.append(cost)
     
+    prices.sort()
+
     total = 0
     cnt = 0
-    for j in range(N):
-        if i == j:
-            continue
-        
-        if total <= B:
-            total += prices[j]
+
+    for price in prices:
+        if total + price <= B:
+            total += price
             cnt += 1
-        ans = max(ans, cnt)
+        else:
+            break
+    ans = max(ans, cnt)
 print(ans)
