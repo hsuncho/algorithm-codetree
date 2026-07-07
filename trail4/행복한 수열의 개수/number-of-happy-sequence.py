7cd3e564243bd1dc0f778ca454b6615c7e42ba1a
Hyeonseon Cho <142369881+hsuncho@users.noreply.github.com>
2026-07-07 21:14:09 +0900

    [Wrong Answer] 행복한 수열의 개수 (67ms, 27MB)

---
+++ trail4/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+answer = 0
+col_seq = [[grid[col][row] for row in range(n)] for col in range(n)]
+
+def is_happy(seq, m):
+    cnt = 1
+    if m == 1:
+        return True
+    for i in range(1, len(seq)):
+        if seq[i-1] == seq[i]:
+            cnt += 1
+            if cnt >= m:
+                return True
+        else:
+            cnt = 1
+    return False
+
+for row in grid:
+    if is_happy(row, m):
+        answer += 1
+
+for col in col_seq:
+    if is_happy(col, m):
+        answer += 1
+
+print(answer)
+
